841fa3dc46309793ab01de93d9ceb097b5eb4d17
Joshua <royaljr@centurylink.net>
2019-04-10 03:48:00 +0000

    Added Bootstrap

--- wp-content/themes/twentynineteen-child/functions.php
+++ wp-content/themes/twentynineteen-child/functions.php
@@ -3,6 +3,6 @@ add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 function my_theme_enqueue_styles() {
  
     wp_enqueue_style( 'child-style',
-        get_stylesheet_directory_uri() . '/style.css'
+        get_stylesheet_directory_uri() . '/css/theme.css'
     );
 }
